9f5e64e00ab7d1c6a6f4c04c0c8239a049a2ef44
Jean-Michel Fortin <jmfortin29@gmail.com>
2023-10-13 23:47:18 -0400

    version try hard avec incertitude : 4275 points

--- bot.py
+++ bot.py
@@ -1,33 +1,44 @@
 from game_message import *
 from actions import *
-from math import atan, sqrt, cos, sin, radians
+from math import acos, sqrt, cos, sin, radians, degrees
 import copy
+import numpy as np
 
 class Bot:
     def __init__(self):
         self.targetted_meteor_ids: list[int] = []
-        self.child_targets: list[Meteor] = []
-        self.next_launch = 0
+        self.target_queue: list[Meteor] = []
+        self.game_bounds: list[int] = []
+        self.large_meteor_uncertainty: float = 0.35
+        self.medium_meteor_uncertainty: float = 1.15
+        self.small_meteor_uncertainty: float = 1.55
         print("Initializing VAUL domination...")
 
     def get_next_move(self, game_message: GameMessage) -> list[LookAtAction | RotateAction | ShootAction]:
         #print(f"Score: {game_message.score}")
+
+        if not self.game_bounds:
+            self.game_bounds = [
+                game_message.cannon.position.x + 0, 
+                game_message.constants.world.width, 
+                0, 
+                game_message.constants.world.height]
 
         # If cannon is in cooldown, we can't do anything
         if game_message.cannon.cooldown > 0:
             return []
 
         # Targetting a meteor
-        if self.child_targets:
-            target_meteor: Meteor = self.child_targets.pop(0)
+        if self.target_queue:
+            target_meteor: Meteor = self.target_queue.pop(0)
         else:
             meteors_collisions: list[Meteor] = self.compute_meteors_collisions(game_message)
-            target_meteor: Meteor = self.select_target_meteor(meteors_collisions, game_message)
+            target_meteor: Meteor = self.select_target_meteor(meteors_collisions)
             if target_meteor is None:
-                return []
-            elif target_meteor.meteorType in [MeteorType.Large, MeteorType.Medium]:
+                return [] 
+            elif target_meteor.meteorType in [MeteorType.Medium, MeteorType.Large]:
                 collision_time: float = self.estimate_collision_time(target_meteor, game_message.tick, game_message)
-                #self.check_for_child_targets(target_meteor, game_message.tick, collision_time, game_message)
+                self.target_child_meteors(target_meteor, collision_time, game_message)
         
         # Updating the targeted ids
         self.update_targeted_ids(target_meteor.id, game_message.meteors)
@@ -52,9 +63,8 @@ class Bot:
         return meteors_collisions
     
 
-    def select_target_meteor(self, meteors: list[Meteor], game_message: GameMessage) -> Meteor:
-        bounds: list[int] = [game_message.cannon.position.x, game_message.constants.world.width, 0, game_message.constants.world.height]
-        reachable_meteors: list[Meteor] = [meteor for meteor in meteors if self.is_inside_bounds(meteor.position, bounds)]
+    def select_target_meteor(self, meteors: list[Meteor]) -> Meteor:
+        reachable_meteors: list[Meteor] = [meteor for meteor in meteors if self.is_inside_bounds(meteor.position)]
         candidate_meteors: list[Meteor] = [meteor for meteor in reachable_meteors if meteor.id not in self.targetted_meteor_ids]
         sorted_candidates: list[Meteor] = sorted(sorted(candidate_meteors, key=lambda meteor: meteor.position.x), key=lambda meteor: meteor.meteorType, reverse=True)
         return sorted_candidates[0] if sorted_candidates else None
@@ -67,28 +77,27 @@ class Bot:
         return launch_time + self.distance(p_rocket, collision_point) / v_rocket
     
 
-    def check_for_child_targets(self, target_meteor: Meteor, launch_time: float, split_time: float, game_message: GameMessage) -> None:
+    def target_child_meteors(self, parent_meteor: Meteor, parent_collision_time: float, game_message: GameMessage) -> None:
 
-        launch_time += game_message.constants.cannonCooldownTicks
-
-        for child in game_message.constants.meteorInfos[target_meteor.meteorType].explodesInto:
-            child_meteor: Meteor = Meteor(id=-1, meteorType=child.meteorType, position=Vector(0,0), velocity=Vector(0,0), size=0)
-            angle: float = radians(child.approximateAngle)
-            speed_ratio: float = game_message.constants.meteorInfos[child.meteorType].approximateSpeed / game_message.constants.meteorInfos[target_meteor.meteorType].approximateSpeed
+        for child in game_message.constants.meteorInfos[parent_meteor.meteorType].explodesInto:
+            # Next rocket will launch after all queued rockets
+            launch_time = game_message.tick + (len(self.target_queue) + 1) * game_message.constants.cannonCooldownTicks
+            
+            meteor_size: float = game_message.constants.meteorInfos[child.meteorType].size
+            child_meteor: Meteor = Meteor(id=-1, meteorType=child.meteorType, position=Vector(0,0), velocity=Vector(0,0), size=meteor_size)
+            split_angle: float = radians(child.approximateAngle)
+            parent_speed: float = np.linalg.norm([parent_meteor.velocity.x, parent_meteor.velocity.y])
+            speed_ratio: float = game_message.constants.meteorInfos[child.meteorType].approximateSpeed / parent_speed
             child_meteor.velocity = Vector(
-                x = speed_ratio * (cos(angle)*target_meteor.velocity.x + sin(angle)*target_meteor.velocity.x), 
-                y = speed_ratio * (cos(angle)*target_meteor.velocity.y - sin(angle)*target_meteor.velocity.y)
+                x = speed_ratio * (cos(split_angle)*parent_meteor.velocity.x - sin(split_angle)*parent_meteor.velocity.y), 
+                y = speed_ratio * (sin(split_angle)*parent_meteor.velocity.x + cos(split_angle)*parent_meteor.velocity.y)
             )
-            # position_at_next_launch: Vector = Vector(
-            #     x = target_meteor.position.x + child_meteor.velocity.x * (launch_time - split_time),
-            #     y = child_meteor.velocity.y * (launch_time - split_time) + target_meteor.position.y
-            # )
-            child_meteor.position = self.get_collision_position_child(target_meteor.position, child_meteor.velocity, game_message.cannon.position, game_message.constants.rockets.speed, split_time, launch_time)
-            child_collision_time: float = self.estimate_collision_time(child_meteor, launch_time, game_message)
-            if self.is_inside_bounds(child_meteor.position, [game_message.cannon.position.x, game_message.constants.world.width, 0, game_message.constants.world.height]):
-                self.child_targets.append(child_meteor)
-                launch_time += game_message.constants.cannonCooldownTicks
-                self.check_for_child_targets(child_meteor, launch_time, child_collision_time, game_message)
+            child_meteor.position = self.get_collision_position(parent_meteor.position, child_meteor.velocity, game_message.cannon.position, 
+                                                                        game_message.constants.rockets.speed, parent_collision_time, launch_time)
+            if self.is_inside_bounds(child_meteor.position) and self.uncertainty_check(child_meteor, parent_meteor.position, game_message):
+                self.target_queue.append(child_meteor)
+                child_collision_time: float = self.estimate_collision_time(child_meteor, launch_time, game_message)
+                self.target_child_meteors(child_meteor, child_collision_time, game_message)
 
 
     def update_targeted_ids(self, target_id, meteors: list[Meteor]) -> None:
@@ -99,42 +108,48 @@ class Bot:
             self.targetted_meteor_ids.append(target_id)
 
 
-    def get_collision_position(self, p_meteor: Vector, v_meteor: Vector, p_rocket: Vector, v_rocket: float) -> (Vector, float):
-        # a,b,c are bad names but used to simplify the equations
-        a: float = (p_rocket.y - p_meteor.y) / (p_rocket.x - p_meteor.x)
-        b: float = (a * v_meteor.x - v_meteor.y) / v_rocket
-        c: float = a**2 - b**2 + 1
-        if c < 0:
-            return None
-        theta: float = 2*atan((sqrt(c) - 1)/(a + b)) 
-        delta_t: float = (p_rocket.x - p_meteor.x) / (v_meteor.x - v_rocket * cos(theta))
-        collision_point: Vector = Vector(x=p_meteor.x + delta_t * v_meteor.x, y=p_meteor.y + delta_t * v_meteor.y)
-        return collision_point
+    def get_collision_position(self, p0_meteor: Vector, v_meteor: Vector, p0_rocket: Vector, v_rocket: float, 
+                                t0_meteor: float = 0, t0_rocket: float = 0) -> Vector:
+        
+        rocket_lead: float = t0_meteor - t0_rocket
+        delta_t: float = 0.0
+        rate: float = 0.02
+        error: float = 1000
+        while abs(error) > 1.0:
+            position = Vector(x=p0_meteor.x + delta_t * v_meteor.x, y=p0_meteor.y + delta_t * v_meteor.y)
+            rocket_travel_distance = (rocket_lead + delta_t) * v_rocket
+            cannon_target_distance = self.distance(position, p0_rocket)
+            error = cannon_target_distance - rocket_travel_distance
+            delta_t += rate * error
+
+        return position
     
 
-    def get_collision_position_child(self, p0_meteor: Vector, v_meteor: Vector, p0_rocket: Vector, v_rocket: float, 
-                                     t0_meteor: float, t0_rocket: float) -> Vector:
-
-        a = p0_rocket.y - p0_meteor.y + t0_meteor*v_meteor.y
-        b = p0_rocket.x - p0_meteor.x + t0_meteor*v_meteor.x
-        vr = v_rocket
-        t1 = t0_rocket
-        vmx = v_meteor.x
-        vmy = v_meteor.y
-        c = -a**2*vmx**2 + a**2*vr**2 + 2*a*b*vmx*vmy - 2*a*t1*vmy*vr**2 - b**2*vmy**2 + b**2*vr**2 - 2*b*t1*vmx*vr**2 + t1**2*vmx**2*vr**2 + t1**2*vmy**2*vr**2
-        if c < 0:
-            return None
-        theta: float = 2*atan((-b*vr + t1*vmx*vr + sqrt(c)) / (a*vmx + a*vr - b*vmy - t1*vmy*vr))
-        tc2: float = (b - t1*vr*cos(theta)) / (vmx - vr*cos(theta))
-        delta_t: float = tc2 - t0_meteor
-        collision_point: Vector = Vector(x=p0_meteor.x + delta_t * v_meteor.x, y=p0_meteor.y + delta_t * v_meteor.y)
-        return collision_point
+    def is_inside_bounds(self, position) -> bool:
+        return (self.game_bounds[0] <= position.x <= self.game_bounds[1] and \
+                self.game_bounds[2] <= position.y <= self.game_bounds[3])
     
 
-    def is_inside_bounds(self, position, bounds) -> bool:
-        return (bounds[0] <= position.x <= bounds[1] and \
-                bounds[2] <= position.y <= bounds[3])
+    def uncertainty_check(self, meteor: Meteor, initial_position: Vector, game_message: GameMessage) -> bool:
+
+        if meteor.meteorType == MeteorType.Large:
+            speed_uncertainty = self.large_meteor_uncertainty
+        elif meteor.meteorType == MeteorType.Medium:
+            speed_uncertainty = self.medium_meteor_uncertainty
+        else:
+            speed_uncertainty = self.small_meteor_uncertainty
+
+        distance_to_travel = self.distance(meteor.position, initial_position)
+        position_uncertainty = distance_to_travel * speed_uncertainty
+        rocket_vector = [meteor.position.x - game_message.cannon.position.x, 
+                            meteor.position.y - game_message.cannon.position.y]
+        rocket_vector /= np.linalg.norm(rocket_vector)
+        meteor_vector = [meteor.velocity.x, meteor.velocity.y]
+        meteor_vector /= np.linalg.norm(meteor_vector)
+        uncertainty = (1 - abs(np.dot(rocket_vector, meteor_vector))) * position_uncertainty
+        return uncertainty < meteor.size + game_message.constants.rockets.size
 
 
     def distance(self, p1, p2) -> float:
         return sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
+    
